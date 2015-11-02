cb9374d9671832033567306875f82b2bfd62024e
zumulv <zumulv@wacai.com>
2015-11-03 01:37:07 +0800

    测试添加注释

--- app/src/main/java/pyystone/androidstudynotes/MainActivity.java
+++ app/src/main/java/pyystone/androidstudynotes/MainActivity.java
@@ -10,6 +10,7 @@ import pyystone.androidstudynotes.ui.UIActivity;
 import pyystone.androidstudynotes.version.VersionMgrActivity;
 
 // 这是主Activity
+// 注释第二行
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
