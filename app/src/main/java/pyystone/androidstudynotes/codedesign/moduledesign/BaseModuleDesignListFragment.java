ee2e5be8a00aa2a4277ebf1e4e012a80a1607925
pyystone <pyystone@vip.qq.com>
2015-12-27 15:19:34 +0800

    补充提交

---
+++ app/src/main/java/pyystone/androidstudynotes/codedesign/moduledesign/BaseModuleDesignListFragment.java
@@ -0,0 +1,62 @@
+package pyystone.androidstudynotes.codedesign.moduledesign;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import pyystone.androidstudynotes.R;
+import pyystone.androidstudynotes.ui.BaseListAdapter;
+import pyystone.androidstudynotes.ui.ListItemData;
+import pyystone.androidstudynotes.util.ApplicationUtil;
+
+/**
+ * Created by pyysotne on 2015/12/25.
+ * email: pyystone@163.com
+ * QQ: 862429936
+ * 不做组件设计的Fragment设计
+ */
+public class BaseModuleDesignListFragment extends Fragment implements AdapterView.OnItemClickListener {
+    private View mRootView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (mRootView != null) {
+            return mRootView;
+        }
+        mRootView = inflater.inflate(R.layout.list_fragment,null);
+        initUI();
+        return mRootView;
+
+    }
+    private void initUI() {
+        ListView listView = (ListView) mRootView.findViewById(R.id.listview);
+        listView.setAdapter(new BaseListAdapter(getListViewData(),getActivity()));
+        listView.setOnItemClickListener(this);
+    }
+
+    public List<ListItemData> getListViewData() {
+        List<ListItemData> data = new ArrayList<>();
+        int count = 20;
+        while(count-- != 0) {
+            ListItemData item = new ListItemData();
+            item.mTitle = "BaseModuleDesignListFragment item" + count;
+            item.mTagId = count;
+            data.add(item);
+        }
+        return data;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        ApplicationUtil.toast(((TextView)view.findViewById(R.id.tv1)).getText().toString());
+    }
+}
---
+++ app/src/main/java/pyystone/androidstudynotes/codedesign/moduledesign/ModlueDesignListFragment.java
@@ -0,0 +1,36 @@
+package pyystone.androidstudynotes.codedesign.moduledesign;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.ListViewAutoScrollHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import pyystone.androidstudynotes.R;
+import pyystone.androidstudynotes.ui.BaseListAdapter;
+
+/**
+ * Created by pyysotne on 2015/12/25.
+ * email: pyystone@163.com
+ * QQ: 862429936
+ * 列表组件
+ */
+public class ModlueDesignListFragment extends Fragment {
+    private View mRootView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mRootView = inflater.inflate(R.layout.list_fragment,null);
+        initUI();
+        return mRootView;
+    }
+
+    private void initUI() {
+        ((ListView)mRootView.findViewById(R.id.listview))
+                .setAdapter(new ModuleDesignListAdapter(new ModuleDesignListModel(),getActivity()));
+    }
+
+}
